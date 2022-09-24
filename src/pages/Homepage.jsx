bba92736bf48ed4fcff68f99e58ab8d29d1ff292
S. M. Mehedi <s.mehedi2022@gmail.com>
2022-09-25 03:30:14 +0600

    hero section demo

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,51 @@
 import React from "react";
+import './css/Homepage.css'
+import vector from '../img/Vector.svg'
 
 const Homepage = () =>{
     return(
-        <h1>Homepage Section</h1>
+        <div className="homepageContainer">
+            <div className="heroSection">
+                <div className="heroSectionLeft">
+                    
+                    <h1>Lorem Ipsum is <span style={{color:"#2793F2"}}><img src={vector} alt="img" className="textBackgroundImg"/>simply dummy</span> text of the printing</h1>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been</p>
+                    <div className="emailBar">
+                        <input type="text" placeholder="Write your email here..."/>
+                        <button>Let's talk</button>
+                    </div>
+                </div>
+                <div className="heroSectionRight">
+                    Image Slider
+                </div>
+            </div>
+            <div className="statistics">
+                <div className="statisticItem">
+                    <h1>20+</h1>
+                    <p>Completed Project</p>
+                    <hr className="lineOne"/>
+                    <hr className="lineTwo"/>
+                </div>
+                <div className="statisticItem">
+                    <h1>15+</h1>
+                    <p>Satisfied Clients</p>
+                    <hr className="lineOne"/>
+                    <hr className="lineTwo"/>
+                </div>
+                <div className="statisticItem">
+                    <h1>6+</h1>
+                    <p>Years of Experience</p>
+                    <hr className="lineOne"/>
+                    <hr className="lineTwo"/>                    
+                </div>
+                <div className="statisticItem">
+                    <h1>99%</h1>
+                    <p>Client Satisfaction</p>
+                </div>
+            </div>
+            <div style={{height:"300px"}}></div>
+        </div>
+        
     );
 }
 
