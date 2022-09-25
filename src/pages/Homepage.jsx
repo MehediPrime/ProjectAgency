718de78c428db93770551e86e634b5a56e854c5d
S. M. Mehedi <s.mehedi2022@gmail.com>
2022-09-26 00:05:15 +0600

    basic animation

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './css/Homepage.css'
 import vector from '../img/Vector.svg';
 import Slider from "./Slider";
 import bgOne from '../img/bgOne.svg'
 const Homepage = () =>{
+
+    // const[revealStatus, setRevealStatus] = useState(false);
+    // const RevealComponentOnScroll = () =>{
+    //     const reveal = document.getElementsByClassName("nonRevealLeft");
+    //     for (let index = 0; index < reveal.length; index++) {
+    //         const innerHeight = window.innerHeight;
+    //         const elementTop = reveal[index].getBoundingClientRect().top;
+    //         console.log(elementTop + " " +innerHeight);
+    //         elementTop < innerHeight-150 ?  setRevealStatus(true) : setRevealStatus(false);
+    //     }
+    // }
+    
+    // window.addEventListener("scroll", RevealComponentOnScroll);
+
+
+    let valueDisplay = document.getElementsByClassName("numCount");
+    let interval = 700;
+    
+    useEffect(()=>{
+        if(valueDisplay){
+            for (const value of valueDisplay) {
+                let startTime = 0;
+                let endTime = parseInt(value.getAttribute("data-val"));
+                let duration = Math.floor(interval/endTime);
+
+                let counter = setInterval(() => {
+                    startTime +=1;
+                    value.textContent = startTime;
+                    if (startTime === endTime) {
+                        clearInterval(counter);
+                    }
+                }, duration);
+
+            }
+        }
+    },[valueDisplay])
+
+    
     return(
         <div className="homepageContainer">
             <div className="bgStyleOne">
@@ -11,9 +49,9 @@ const Homepage = () =>{
             </div>
             <div className="bgStyleTwo"></div>
             <div className="heroSection">
-                <div className="heroSectionLeft">
-                    <h1>Lorem Ipsum is <span style={{color:"#2793F2"}}><img src={vector} alt="img" className="textBackgroundImg"/>simply dummy</span> text of the printing</h1>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been</p>
+                <div className='heroSectionLeft' >
+                    <h1 className="animationLR">Lorem Ipsum is <span style={{color:"#2793F2"}}><img src={vector} alt="img" className="textBackgroundImg"/>simply dummy</span> text of the printing</h1>
+                    <p className="animationLR">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been</p>
                     <div className="emailBar">
                         <input type="text" placeholder="Write your email here..."/>
                         <button>Let's talk</button>
@@ -25,25 +63,25 @@ const Homepage = () =>{
             </div>
             <div className="statistics">
                 <div className="statisticItem">
-                    <h1>20+</h1>
+                    <h1><span className="numCount" data-val="30">0</span>+</h1>
                     <p>Completed Project</p>
                     <hr className="lineOne"/>
                     <hr className="lineTwo"/>
                 </div>
                 <div className="statisticItem">
-                    <h1>15+</h1>
+                    <h1><span className="numCount" data-val="20">0</span>+</h1>
                     <p>Satisfied Clients</p>
                     <hr className="lineOne"/>
                     <hr className="lineTwo"/>
                 </div>
                 <div className="statisticItem">
-                    <h1>6+</h1>
+                <h1><span className="numCount" data-val="6">0</span>+</h1>
                     <p>Years of Experience</p>
                     <hr className="lineOne"/>
                     <hr className="lineTwo"/>                    
                 </div>
                 <div className="statisticItem">
-                    <h1>99%</h1>
+                <h1><span className="numCount" data-val="99">0</span>%</h1>
                     <p>Client Satisfaction</p>
                 </div>
             </div>
